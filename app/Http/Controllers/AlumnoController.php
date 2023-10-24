e8dad3eb42e3ed95364492da8617421188254ee6
reivhutt <reivhutt@gmail.com>
2023-10-23 20:54:06 -0500

    Controladores en Laravel

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\AlumnoRepository;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() 
+    {
+        $alumnos = $this->alumnos->obtenerAlumnos(); 
+        return view('alumnos.lista', ['alumnos' => $alumnos]); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('alumnos.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->alumnos->InsertarAlumnos($request);
+        return redirect()->action([AlumnoController::class, 'index']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $alumno = $this->alumnos->obtenerAlumnoPorId($id); 
+        return view('alumnos.ver', ['alumno' => $alumno])
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $alumno = $this->alumnos->obtenerAlumnoPorId($id);
+        return view('alumnos.editar', ['alumno' => $alumno]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $this->alumnos->actualizarAlumno($request, $id);
+        return redirect()->action([AlumnoController::class, 'index']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $this->alumnos->eliminarAlumno($id);
+        return redirect()->action([AlumnoController::class, 'index']);
+    }
+}
